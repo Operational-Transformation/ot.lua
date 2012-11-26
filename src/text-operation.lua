15bacde555415a9c9fec8e0d4147fdd74f5fc185
Tim Baumann <tim@timbaumann.info>
2012-11-26 08:42:41 +0100

    comments in text-operation.lua

--- src/text-operation.lua
+++ src/text-operation.lua
@@ -1,6 +1,14 @@
 local TextOperation = {}
 TextOperation.__index = TextOperation
 
+-- Operation are essentially lists of ops. There are three types of ops:
+--
+-- * Retain ops: Advance the cursor position by a given number of characters.
+--   Represented by positive ints.
+-- * Insert ops: Insert a given string at the current cursor position.
+--   Represented by strings.
+-- * Delete ops: Delete the next n characters. Represented by negative ints.
+
 local function isRetain(op)
   return type(op) == "number" and op > 0
 end
@@ -23,6 +31,7 @@ local function opLen(op)
   end
 end
 
+-- Shortens an op by the given number of characters
 local function shorten(op, by)
   if type(op) == "string" then
     return string.sub(op, 1 + by)
@@ -33,6 +42,9 @@ local function shorten(op, by)
   end
 end
 
+-- Shortens a pair of ops. The result will be a tuple where the shorter of the
+-- both ops is replaced by null and the longer one is shorted by the length of
+-- the shorter one.
 local function shortenOps(a, b)
   local lenA = opLen(a)
   local lenB = opLen(b)
@@ -45,6 +57,12 @@ local function shortenOps(a, b)
   end
 end
 
+-- Constructs an empty operation
+-- 
+-- When an operation is applied to an input string, you can think of this as
+-- if an imaginary cursor runs over the entire string and skips over some
+-- parts, deletes some parts and inserts characters at some positions. These
+-- actions (skip/delete/insert) are stored as an array in the "ops" property.
 function TextOperation.new(ops)
   return setmetatable({ ops = ops or {} }, TextOperation)
 end
@@ -55,6 +73,7 @@ setmetatable(TextOperation, {
   end
 })
 
+-- Constructs a TextOperation from a JSON document.
 function TextOperation.fromJSON(jsonOps)
   local operation = TextOperation.new()
   for i=1, #jsonOps do
@@ -70,10 +89,14 @@ function TextOperation.fromJSON(jsonOps)
   return operation
 end
 
+-- Encode a TextOperation as a JSON object. For example, the operation
+-- `TextOperation.new():retain(3):insert("Lorem"):delete(5)` is encoded as the
+-- JSON object `[3, "Lorem", -5]` (or `{ 3, "Lorem", -5 }` in Lua)
 function TextOperation:toJSON()
   return self.ops
 end
 
+-- Tests two operations for equality.
 function TextOperation:__eq(other)
   if #self.ops ~= #other.ops then
     return false
@@ -88,6 +111,7 @@ function TextOperation:__eq(other)
   return true
 end
 
+-- Skip over a given number of characters.
 function TextOperation:retain(n)
   local ops = self.ops
   if n ~= 0 then
@@ -100,6 +124,7 @@ function TextOperation:retain(n)
   return self
 end
 
+-- Insert a string at the current position.
 function TextOperation:insert(s)
   local ops = self.ops
   if #s ~= 0 then
@@ -112,6 +137,7 @@ function TextOperation:insert(s)
   return self
 end
 
+-- Delete a string at the current position.
 function TextOperation:delete(n)
   local ops = self.ops
   if n ~= 0 then
@@ -127,6 +153,7 @@ function TextOperation:delete(n)
   return self
 end
 
+-- How many characters were added by the operations vs. deleted?
 function TextOperation:lenDifference()
   local s = 0
   for i=1, #self.ops do
@@ -140,6 +167,8 @@ function TextOperation:lenDifference()
   return s
 end
 
+-- Apply an operation to a string, returning a new string. Throws an error if
+-- there's a mismatch between the input string and the operation length.
 function TextOperation:__call(doc)
   local parts = {}
   local len = 1
@@ -169,6 +198,10 @@ function TextOperation:__call(doc)
   return table.concat(parts, '')
 end
 
+-- Computes the inverse of an operation. The inverse of an operation is the
+-- operation that reverts the effects of the operation, e.g. when you have an
+-- operation 'insert("hello "); skip(6);' then the inverse is 'delete("hello ");
+-- skip(6);'. The inverse should be used for implementing undo.
 function TextOperation:invert(doc)
   local len = 1
   local inverse = TextOperation()
@@ -189,6 +222,10 @@ function TextOperation:invert(doc)
   return inverse
 end
 
+-- Compose merges two consecutive operations into one operation, that
+-- preserves the changes of both. Or, in other words, for each input string S
+-- and a pair of consecutive operations A and B,
+-- apply(apply(S, A), B) = apply(S, compose(A, B)) must hold.
 function TextOperation:compose(other)
   local ia = 1
   local ib = 1
@@ -241,6 +278,10 @@ function TextOperation:compose(other)
   return operation
 end
 
+-- Transform takes two operations A and B that happened concurrently and
+-- returns two operations A' and B' such that
+-- apply(apply(S, A), B') = apply(apply(S, B), A'). This function is the heart
+-- of OT.
 function TextOperation.transform(operationA, operationB)
   local ia = 1
   local ib = 1
@@ -297,4 +338,5 @@ function TextOperation.transform(operationA, operationB)
   return aPrime, bPrime
 end
 
+-- Export TextOperation
 return TextOperation
